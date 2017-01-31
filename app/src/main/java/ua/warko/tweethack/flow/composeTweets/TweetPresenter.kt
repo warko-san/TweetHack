c81fdcd8e291fb00cfa6a9a76d0546e68ec47d69
dima.sholom <dima.sholom@yalantis.com>
2017-01-31 19:59:50 +0200

    Stated to implement algorithm

--- app/src/main/java/ua/warko/tweethack/flow/composeTweets/TweetPresenter.kt
+++ app/src/main/java/ua/warko/tweethack/flow/composeTweets/TweetPresenter.kt
@@ -1,6 +1,7 @@
 package ua.warko.tweethack.flow.composeTweets
 
 import com.yalantis.base.BasePresenterImplementation
+import java.util.*
 
 /**
  * Created by dmitry on 1/30/17.
@@ -8,6 +9,20 @@ import com.yalantis.base.BasePresenterImplementation
 class TweetPresenter : BasePresenterImplementation<TweetContract.View>(), TweetContract.Presenter {
 
     override fun calculateAndSendTweets(text: String) {
+        var tweetList = ArrayList<String>()
+        var stringBuilder = StringBuilder(text)
+        while (stringBuilder.isNotEmpty()) {
+            val tweet = calculateTweets(stringBuilder)
+            tweetList.add(tweet)
+            stringBuilder.delete(0, tweet.length)
+        }
+    }
 
+    private fun calculateTweets(text: StringBuilder) : String {
+        for (i in 139..120) {
+            if (text[i].equals(" "))
+                text.substring(0, i)
+        }
+        return text.toString()
     }
 }
